e3382bc183112a404496cb745324f0f03dc79719
jmjackson <jarceluz@gmail.com>
2015-07-29 21:20:46 -0700

    vista crear usuario

--- resources/views/usuario/create.blade.php
+++ resources/views/usuario/create.blade.php
@@ -1,19 +1,37 @@
 @extends('layouts.admin')
 
 @section('contenido')
-<form action="" method="get">
-	<div class="form-group">
-		<label for="">Nombre</label>
-		<input type="text" class="form-control">
-	</div>
-	<div class="form-group">
-		<label for="">Correo</label>
-		<input type="text" class="form-control">
-	</div>
-		<div class="form-group">
-		<label for="">Password</label>
-		<input type="text" class="form-control">
-	</div>
-	<button class="btn btn-primary">Registrar</button>
-</form>
+
+
+{!!Form::open()!!}
+
+<div class="form-group">
+	{!!Form::label('Nombre')!!}
+	{!!Form::text('nombre',null,['class'=>'form-control','placeholder'=>'Ingrese un nombre'])!!}
+</div>
+<div class="form-group">
+	{!!Form::label('Direccion')!!}
+	{!!Form::text('direccion',null,['class'=>'form-control','placeholder'=>'Ingrese una direccion'])!!}
+</div>
+<div class="form-group">
+	{!!Form::label('Telefono')!!}
+	{!!Form::text('telefono',null,['class'=>'form-control','placeholder'=>'Ingrese un telefono'])!!}
+</div>
+<div class="form-group">
+	{!!Form::label('Usuario')!!}
+	{!!Form::text('usuario',null,['class'=>'form-control','placeholder'=>'Ingrese un usuario'])!!}
+</div>
+
+<div class="form-group">
+	{!!Form::label('Correo')!!}
+	{!!Form::email('correo',null,['class'=>'form-control','placeholder'=>'Ingrese un email',])!!}
+</div>
+<div class="form-group">
+	{!!Form::label('Password')!!}
+	{!!Form::password('password',null,['class'=>'form-control'])!!}
+</div>
+<div class="form-group">
+	{!!Form::submit('enviar',null,['class'=>'btn btn-primary'])!!}
+</div>
+{!!Form::close()!!}
 @stop
